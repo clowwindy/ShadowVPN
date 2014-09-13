8719c5658564bda1be614d0ebb902db070dd780b
clowwindy <clowwindy42@gmail.com>
2014-09-13 23:42:50 +0800

    remove useless comments

--- src/crypto.h
+++ src/crypto.h
@@ -11,11 +11,10 @@ int shadowvpn_encrypt(unsigned char *c, unsigned char *m,
 
 int shadowvpn_decrypt(unsigned char *m, unsigned char *c,
                       unsigned long long clen);
-/*
-   MAC IV1 (IV2 NONCE CIPHER_TEXT)
-*/
+
 #define SHADOWVPN_KEY_LEN 32
 #define SHADOWVPN_ZERO_BYTES 32
 #define SHADOWVPN_OVERHEAD_LEN 24
 #define SHADOWVPN_PACKET_OFFSET 8
+
 #endif
