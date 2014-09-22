011f74dfc3123107f10367bf136b701c4ab053df
clowwindy <clowwindy42@gmail.com>
2014-09-23 01:13:48 +0800

    add some performance tests

---
+++ src/crypto_perf.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "shadowvpn.h"
+
+int main(int argc, char **argv) {
+  int i;
+
+  char *tun_buf = malloc(1532);
+  char *udp_buf = malloc(1532);
+  memset(tun_buf, 0, SHADOWVPN_ZERO_BYTES);
+  memset(udp_buf, 0, SHADOWVPN_ZERO_BYTES);
+
+  for(i = 0; i < 1000 * 100; i++) {
+      crypto_decrypt(tun_buf, udp_buf, 1500);
+  }
+  return 0;
+}
