c0766dfa2cdadb9405499c099f05ee4882bd2299
Han Chen <chen1324+git@gmail.com>
2014-10-07 23:11:44 +0800

    Restore linux server_up.sh

--- samples/server_up.sh
+++ samples/server_up.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-set -x
 # example server up script
 # will be executed when server is up
 
@@ -8,21 +7,21 @@ set -x
 # as environment variables, except password
 
 # turn on IP forwarding
-sysctl -w net.inet.ip.forwarding=1
+sysctl -w net.ipv4.ip_forward=1
 
 # configure IP address and MTU of VPN interface
-ifconfig $intf 10.7.0.1 10.7.0.1 netmask 255.255.255.0 mtu $mtu up
-#ifconfig $intf mtu $mtu
+ifconfig $intf 10.7.0.1 netmask 255.255.255.0
+ifconfig $intf mtu $mtu
 
 # turn on NAT over eth0 and VPN
 # if you use other interface name that eth0, replace eth0 with it
-#iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-#iptables -A FORWARD -i eth0 -o $intf -m state --state RELATED,ESTABLISHED -j ACCEPT
-#iptables -A FORWARD -i $intf -o eth0 -j ACCEPT
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A FORWARD -i eth0 -o $intf -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i $intf -o eth0 -j ACCEPT
 
 # turn on MSS fix
 # MSS = MTU - TCP header - IP header
-#mss=$(($mtu - 40))
-#iptables -t mangle -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss $mss
+mss=$(($mtu - 40))
+iptables -t mangle -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss $mss
 
 echo $0 done
