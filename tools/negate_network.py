0dc813cdc5c130b901c8ebfb755b68cf15b36d50
clowwindy <clowwindy42@gmail.com>
2014-10-15 16:37:20 +0800

    lint code

--- tools/negate_network.py
+++ tools/negate_network.py
@@ -1,24 +1,45 @@
 #!/usr/bin/env python3
+#
+# Copyright (c) 2014 clowwindy
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-from ipaddress import ip_address, ip_network
+from ipaddress import ip_network
 import sys
 
 s = [ip_network('0.0.0.0/0')]
 
 for line in sys.stdin:
-    ip = line.strip()
-    print(ip, file=sys.stderr)
-    # TODO use binary search
-    subnet = ip_network(ip)
+    line = line.strip()
+    print(line, file=sys.stderr)
+    ex_item = ip_network(line)
     new_s = []
-    for net in s:
-        if net.overlaps(subnet):
-            for ss in net.address_exclude(subnet):
-                new_s.append(ss)
+    # TODO use binary search
+    for subnet in s:
+        if subnet.overlaps(ex_item):
+            for sub_subnet in subnet.address_exclude(ex_item):
+                new_s.append(sub_subnet)
         else:
-            new_s.append(net)
+            new_s.append(subnet)
     s = new_s
 
 s.sort()
-for ip in s:
-    print(ip)
+
+for subnet in s:
+    print(subnet)
