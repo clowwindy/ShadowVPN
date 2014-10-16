77a61190675709566c208ca247ea33ff4c8dbe23
rebuilt <xinchang@californiacolleges.edu>
2014-10-16 10:57:51 -0500

    remove useless file

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(shadowvpn):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="shadowvpn" command="/usr/bin/shadowvpn"
--- debian/shadowvpn.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the shadowvpn package
-#
-0 4	* * *	root	[ -x /usr/bin/shadowvpn_maintenance ] && /usr/bin/shadowvpn_maintenance
