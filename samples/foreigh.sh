8dd18eefb6cd0023d3cb096c42197282c1cb282c
clowwindy <clowwindy42@gmail.com>
2015-01-29 02:01:43 +0800

    add foreign.sh

---
+++ samples/foreigh.sh
@@ -0,0 +1,1 @@
+../tools/foreign.sh
--- tools/gen_foreign.sh
+++ tools/gen_foreign.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 cat private.txt chnroute.txt | python3 negate_network.py > foreign.txt
+cat foreign.txt | python3 gen_foreign_sh.py > foreign.sh
+
