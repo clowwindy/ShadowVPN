30dbdfd6d203d05721117036ef1f4b46b9634524
Riobard <me@riobard.com>
2015-07-23 15:25:30 +0800

    Improved config and script files
    - Use iproute2 instead of net-tools (ifconfig) to set MTU and tunnel
      subnet since net-tools has long been deprecated.
    - Use /31 subnets for point-to-point VPN links instead of /24 per RFC
      3021.
    - Move MTU and subnet config from scripts to config files so that
      multiple instances of server/client can share the same pair of up/down
      scripts.
    - Slightly improved comments.

--- samples/client_down.sh
+++ samples/client_down.sh
@@ -1,24 +1,20 @@
 #!/bin/sh
 
-# example client down script
-# will be executed when client is down
+# This script will be executed when client is down. All key value pairs (except
+# password) in ShadowVPN config file will be passed to this script as
+# environment variables.
 
-# all key value pairs in ShadowVPN config file will be passed to this script
-# as environment variables, except password
-
-# uncomment if you want to turn off IP forwarding
-# sysctl -w net.ipv4.ip_forward=0
+# Turn off IP forwarding
+#sysctl -w net.ipv4.ip_forward=0
 
 # turn off NAT over VPN
 iptables -t nat -D POSTROUTING -o $intf -j MASQUERADE
 iptables -D FORWARD -i $intf -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -D FORWARD -o $intf -j ACCEPT
 
-# change routing table
-echo rollback the default route
+# Restore routing table
 ip route del $server
-ip route del 0.0.0.0/1
-ip route del 128.0.0.0/1
-echo default route changed to original route
+ip route del   0/1
+ip route del 128/1
 
 echo $0 done
--- samples/client_up.sh
+++ samples/client_up.sh
@@ -1,32 +1,26 @@
 #!/bin/sh
 
-# example client up script
-# will be executed when client is up
+# This script will be executed when client is up. All key value pairs (except
+# password) in ShadowVPN config file will be passed to this script as
+# environment variables.
 
-# all key value pairs in ShadowVPN config file will be passed to this script
-# as environment variables, except password
-
-# turn on IP forwarding
+# Turn on IP forwarding
 sysctl -w net.ipv4.ip_forward=1
 
-# configure IP address and MTU of VPN interface
-ifconfig $intf 10.7.0.2 netmask 255.255.255.0
-ifconfig $intf mtu $mtu
+# Configure IP address and MTU of VPN interface
+ip addr add $net dev $intf
+ip link set $intf mtu $mtu
 
-# get current gateway
-echo get the original default gateway
-gateway=$(ip route show 0/0 | grep via | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}')
-
-# turn on NAT over VPN
+# Turn on NAT over VPN
 iptables -t nat -A POSTROUTING -o $intf -j MASQUERADE
 iptables -I FORWARD 1 -i $intf -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -I FORWARD 1 -o $intf -j ACCEPT
 
-# change routing table
-echo override the default route
-ip route add $server via $gateway
-ip route add 0.0.0.0/1 via 10.7.0.1
-ip route add 128.0.0.0/1 via 10.7.0.1
-echo default route changed to 10.7.0.1
+# Direct route to VPN server's public IP via current gateway
+ip route add $server via $(ip route show 0/0 | awk '{print $3}')
+
+# Shadow default route using two /1 subnets
+ip route add   0/1 dev $intf
+ip route add 128/1 dev $intf
 
 echo $0 done
