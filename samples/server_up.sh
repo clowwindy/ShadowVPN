9cdbc8df51785f9bea79456a3bfb6f85a383c615
Riobard <me@riobard.com>
2015-07-23 22:10:48 +0800

    Added description for server_up.sh script

--- samples/server_up.sh
+++ samples/server_up.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# This script will be executed when server is up. All key value pairs (except
+# password) in ShadowVPN config file will be passed to this script as
+# environment variables.
 
 # Turn on IP forwarding
 sysctl -w net.ipv4.ip_forward=1
